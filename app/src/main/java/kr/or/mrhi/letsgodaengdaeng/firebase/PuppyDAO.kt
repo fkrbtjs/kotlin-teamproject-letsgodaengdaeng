8d1414df010eed5fcb8a0648f92c08f62e64517f
21yong0525@gmail.com <21yong0525@gmail.com>
2022-11-29 15:33:44 +0900

    [CREATE] anim, drawable 추가

---
+++ app/src/main/java/kr/or/mrhi/letsgodaengdaeng/firebase/PuppyDAO.kt
@@ -0,0 +1,26 @@
+package kr.or.mrhi.letsgodaengdaeng.firebase
+
+import com.google.android.gms.tasks.Task
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.ktx.Firebase
+import com.google.firebase.storage.FirebaseStorage
+import com.google.firebase.storage.ktx.storage
+import kr.or.mrhi.letsgodaengdaeng.dataClass.Puppy
+import kr.or.mrhi.letsgodaengdaeng.dataClass.User
+
+class PuppyDAO {
+    var databaseReference: DatabaseReference? = null
+    var storage: FirebaseStorage? = null
+
+    init {
+        val db: FirebaseDatabase = FirebaseDatabase.getInstance()
+        databaseReference = db.getReference("puppy")
+        storage = Firebase.storage
+    }
+
+    fun signUpPuppy(userCode: String, puppy: Puppy): Task<Void> {
+        return databaseReference!!.child(userCode).setValue(puppy)
+    }
+
+}
