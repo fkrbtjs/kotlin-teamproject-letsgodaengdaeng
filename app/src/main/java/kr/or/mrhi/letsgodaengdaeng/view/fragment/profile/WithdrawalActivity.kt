369bb9aed523d22c933a455d1776396a3521c7fd
woong27 <rjsdnd27@gmail.com>
2022-12-01 16:15:32 +0900

    [UPDATE} Membership Withdrawal

--- app/src/main/java/kr/or/mrhi/letsgodaengdaeng/view/fragment/profile/WithdrawalActivity.kt
+++ app/src/main/java/kr/or/mrhi/letsgodaengdaeng/view/fragment/profile/WithdrawalActivity.kt
@@ -18,62 +18,65 @@ import com.google.firebase.database.ktx.getValue
 import kr.or.mrhi.letsgodaengdaeng.R
 import kr.or.mrhi.letsgodaengdaeng.dataClass.User
 import kr.or.mrhi.letsgodaengdaeng.databinding.ActivityWithdrawalBinding
+import kr.or.mrhi.letsgodaengdaeng.databinding.DialogLayoutBinding
 import kr.or.mrhi.letsgodaengdaeng.firebase.UserDAO
 import kr.or.mrhi.letsgodaengdaeng.view.activity.LoginActivity
+import kr.or.mrhi.letsgodaengdaeng.view.activity.MainActivity
 
 class WithdrawalActivity : AppCompatActivity() {
     lateinit var binding: ActivityWithdrawalBinding
+    lateinit var bindingDialog: DialogLayoutBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityWithdrawalBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        bindingDialog = DialogLayoutBinding.inflate(layoutInflater)
 
         binding.cbConsent.setOnClickListener {
-            if(binding.cbConsent.isChecked) {
+            if (binding.cbConsent.isChecked) {
                 binding.btnWithdrawal.visibility = View.VISIBLE
                 Log.d("profileactivity", "나와라")
-            }else{
+            } else {
                 binding.btnWithdrawal.visibility = View.INVISIBLE
                 Log.d("profileactivity", "숨어라")
             }
         }
 
-        binding.btnWithdrawal.setOnClickListener{
+        binding.btnWithdrawal.setOnClickListener {
             val intent = Intent(this, LoginActivity::class.java)
-            val dialog: AlertDialog.Builder = AlertDialog.Builder(this, android.R.style.Theme_DeviceDefault_Light_Dialog_NoActionBar_MinWidth)
+            val dialog: AlertDialog.Builder = AlertDialog.Builder(
+                this,
+                android.R.style.Theme_DeviceDefault_Light_Dialog_NoActionBar_MinWidth
+            )
             val userDAO = UserDAO()
+
             dialog.setTitle("회원탈퇴")
             dialog.setMessage("패스워드를 입력해주세요")
-            dialog.setView(R.layout.dialog_layout)
-            dialog.setPositiveButton("예", DialogInterface.OnClickListener{ dialog, which ->
-                userDAO.selectUser()?.addValueEventListener(object : ValueEventListener{
-                    override fun onDataChange(snapshot: DataSnapshot) {
-                        val text = findViewById<EditText>(R.id.edt_dialog)
-                        for(userData in snapshot.children){
-                            val user = userData.getValue(User::class.java) // json 파일을 클래스파일로 만든다
-                            Log.d("letsgodaengdaeng", "${user.toString()}")
-                            if (user?.password.toString() == "${text.setText("123asd")}"){
-                                Toast.makeText(this@WithdrawalActivity, "로그아웃 되었습니다", Toast.LENGTH_SHORT).show()
-                                Log.e("letsgodaengdaeng", "로그아웃 ${user?.password.toString() == "123asd"}")
-                            }else{
-                                Log.e("letsgodaengdaeng", "로그아웃실패")
-                                dialog.dismiss()
+            dialog.setView(bindingDialog.root)
+            dialog.setPositiveButton("예", DialogInterface.OnClickListener { dialog, which ->
+                userDAO.selectUser(MainActivity.userCode!!)
+                    ?.addListenerForSingleValueEvent(object : ValueEventListener {
+                        override fun onDataChange(snapshot: DataSnapshot) {
+                            val user: User? = snapshot.getValue(User::class.java)
+
+                            if (user?.password == bindingDialog.edtDialog.text.toString()) {
+                                Log.e("letsgodaengdaeng", "delete")
+                                deleteData()
+                                startActivity(intent)
+                                Toast.makeText(this@WithdrawalActivity, "회원탈퇴 되었습니다. 이용해주셔서 감사합니다.", Toast.LENGTH_SHORT).show()
+                            } else {
+                                Log.e("letsgodaengdaeng", "delete no")
+                                Toast.makeText(this@WithdrawalActivity, "패스워드가 일치하지 않습니다.", Toast.LENGTH_SHORT).show()
+
                             }
-                        }// end of for
-                    }
-                    override fun onCancelled(error: DatabaseError) {
-                        Log.d("letsgodaengdaeng", "${error.toString()}")
-                    }
-                })
-
-
-
-//                deleteData()
-//                startActivity(intent)
-
+                        }
+                        override fun onCancelled(error: DatabaseError) {
+                            Log.d("letsgodaengdaeng", "${error.toString()}")
+                        }
+                    })
                 finish()
             })
-            dialog.setNegativeButton("아니오", DialogInterface.OnClickListener{ dialog, which ->
+            dialog.setNegativeButton("아니오", DialogInterface.OnClickListener { dialog, which ->
                 dialog.dismiss()
             })
             dialog.show()
@@ -82,7 +85,7 @@ class WithdrawalActivity : AppCompatActivity() {
 
     private fun deleteData() {
         val userDAO = UserDAO()
-        userDAO.deleteUser("2uWa4YHCNfPg0qMFjRtjvMFHtfg2").addOnSuccessListener {
+        userDAO.deleteUser(MainActivity.userCode!!).addOnSuccessListener {
             Toast.makeText(applicationContext, "User delete success", Toast.LENGTH_SHORT).show()
             Log.d("firebasecrud", "User delete success")
         }.addOnFailureListener {
