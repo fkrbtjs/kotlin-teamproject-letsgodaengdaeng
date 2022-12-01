082b51e86ba7d49a154e475efba58795617c8e68
woong27 <rjsdnd27@gmail.com>
2022-12-01 15:11:38 +0900

    [UPDATE] 유저정보 액티비티 수정

--- app/src/main/java/kr/or/mrhi/letsgodaengdaeng/view/fragment/profile/InfoActivity.kt
+++ app/src/main/java/kr/or/mrhi/letsgodaengdaeng/view/fragment/profile/InfoActivity.kt
@@ -1,13 +1,37 @@
 package kr.or.mrhi.letsgodaengdaeng.view.fragment.profile
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
 import kr.or.mrhi.letsgodaengdaeng.R
+import kr.or.mrhi.letsgodaengdaeng.databinding.ActivityInfoBinding
 
 class InfoActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val binding =
-        setContentView(R.layout.activity_info)
+        val binding = ActivityInfoBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        //Actionbar -> Toolbar 변경
+        setSupportActionBar(binding.toolInfo)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+        binding.ivUpdate.setOnClickListener{
+            val intent = Intent(this, UpdateActivity::class.java)
+            startActivity(intent)
+        }
+    }
+
+
+    //뒤로가기버튼
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item.itemId){
+            android.R.id.home -> {
+                finish()
+                return true
+            }
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
