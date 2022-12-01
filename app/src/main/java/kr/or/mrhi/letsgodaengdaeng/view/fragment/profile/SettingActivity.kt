9129fffd7e4537e21100c9e69dacb18e08e74d00
woong27 <rjsdnd27@gmail.com>
2022-12-01 15:12:01 +0900

    [UPDATE] 유저설정 액티비티 수정

--- app/src/main/java/kr/or/mrhi/letsgodaengdaeng/view/fragment/profile/SettingActivity.kt
+++ app/src/main/java/kr/or/mrhi/letsgodaengdaeng/view/fragment/profile/SettingActivity.kt
@@ -1,9 +1,16 @@
 package kr.or.mrhi.letsgodaengdaeng.view.fragment.profile
 
+import android.content.DialogInterface
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import kr.or.mrhi.letsgodaengdaeng.R
 import kr.or.mrhi.letsgodaengdaeng.databinding.ActivitySettingBinding
+import kr.or.mrhi.letsgodaengdaeng.view.activity.LoginActivity
+import kr.or.mrhi.letsgodaengdaeng.view.activity.MainActivity
 
 class SettingActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -15,5 +22,36 @@ class SettingActivity : AppCompatActivity() {
         setSupportActionBar(binding.toolSet)
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
 
+        // 로그아웃을 눌렀을때 AlertDialog 띄우기
+        binding.tvLogout.setOnClickListener {
+            val intent = Intent(this, LoginActivity::class.java)
+            val dialog: AlertDialog.Builder = AlertDialog.Builder(this, android.R.style.Theme_DeviceDefault_Light_Dialog_NoActionBar_MinWidth)
+            dialog.setTitle("로그아웃")
+            dialog.setMessage("로그아웃 하시겠습니까?")
+            dialog.setPositiveButton("예", DialogInterface.OnClickListener{ dialog, which ->
+                startActivity(intent)
+                Toast.makeText(this, "로그아웃 되었습니다", Toast.LENGTH_SHORT).show()
+                finish()
+            })
+            dialog.setNegativeButton("아니오", DialogInterface.OnClickListener{ dialog, which ->
+                dialog.dismiss()
+            })
+            dialog.show()
+        }
+
+        binding.btnWithdrawal.setOnClickListener {
+            val intent = Intent(this, WithdrawalActivity::class.java)
+            startActivity(intent)
+        }
+    }
+    //백버튼을 눌렀을떄 이동할 경로 지정
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item.itemId){
+            android.R.id.home -> {
+                finish()
+                return true
+            }
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
